0dbfd023adf687ffd48673d624a5131c2060cb08
bertrandmer <97544521+bertrandmer@users.noreply.github.com>
2022-06-21 18:52:44 -0400

    Add files via upload

---
+++ createFile.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+fs.writeFile("HelloWorld.txt", "Hello World!", (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Successfully wrote file!");
+    }
+  });
